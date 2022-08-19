15b6b49eedd5eabf6cf0ae5c5f648d4c4323cea0
Tee <i@lado.me>
2022-08-19 11:51:21 +0800

    fix logic

--- record/Tee_move play cursor to mouse modifiers during recording.lua
+++ record/Tee_move play cursor to mouse modifiers during recording.lua
@@ -36,35 +36,35 @@ else
     msg2 = "This script only use in Ruler or MIDI ruler in mouse modifiers"
 end
 
-if (reaper.APIExists("BR_GetMouseCursorContext") == false) then
-    reaper.ShowMessageBox(msg1, title1, 0)
-end
-
-local function move_cursor_to_mouse()
-    window, segment, details = reaper.BR_GetMouseCursorContext()
-    if (window == "midi_editor" and segment == "ruler") then
-        hwnd_focus = reaper.BR_Win32_GetFocus()
-        reaper.MIDIEditor_OnCommand(hwnd_focus, 40443) -- move cursor to mouse
-    elseif (window == "ruler") then
-        reaper.Main_OnCommand(40513, 0) -- move cursor to mouse
-    else
-        reaper.ShowMessageBox(msg2, title2, 0)
-    end
-end
-
 reaper.PreventUIRefresh(1)
 
-local project_status = reaper.GetAllProjectPlayStates(0)
-
--- project_status
--- 5 recording; 6 recording & pause
-
-if (project_status == 5) or (project_status == 6) then
-    reaper.Main_OnCommand(40667, 0) -- Transport: Stop (save all recorded media)
-    move_cursor_to_mouse()
-    reaper.Main_OnCommand(1013, 0) -- Transport: Record
+if (reaper.APIExists("BR_GetMouseCursorContext") == false) then
+    reaper.ShowMessageBox(msg1, title1, 0)
 else
-    move_cursor_to_mouse()
+    local function move_cursor_to_mouse()
+        window, segment, details = reaper.BR_GetMouseCursorContext()
+        if (window == "midi_editor" and segment == "ruler") then
+            hwnd_focus = reaper.BR_Win32_GetFocus()
+            reaper.MIDIEditor_OnCommand(hwnd_focus, 40443) -- move cursor to mouse
+        elseif (window == "ruler") then
+            reaper.Main_OnCommand(40513, 0) -- move cursor to mouse
+        else
+            reaper.ShowMessageBox(msg2, title2, 0)
+        end
+    end
+
+    local project_status = reaper.GetAllProjectPlayStates(0)
+
+    -- project_status
+    -- 5 recording; 6 recording & pause
+
+    if (project_status == 5) or (project_status == 6) then
+        reaper.Main_OnCommand(40667, 0) -- Transport: Stop (save all recorded media)
+        move_cursor_to_mouse()
+        reaper.Main_OnCommand(1013, 0) -- Transport: Record
+    else
+        move_cursor_to_mouse()
+    end
 end
 
 reaper.PreventUIRefresh(-1)
